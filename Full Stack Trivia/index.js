b9bcb3ca2c4d685d2e9f98effb934f73cf78ddeb
Jason <github@jasongetsemail.com>
2023-08-18 12:09:50 -0400

    Add score tracking and player turns. Make answer a global variable on the server to follow JS conventions.

--- Full Stack Trivia/index.js
+++ Full Stack Trivia/index.js
@@ -3,6 +3,11 @@ import bodyParser from "body-parser";
 import axios from "axios";
 const app = express();
 let board = [];
+let score;
+let chosenQuestion;
+let player1Score = 0;
+let player2Score = 0;
+let playerTurn = 1;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -13,6 +18,7 @@ app.get("/", async (req, res) => {
   let categories;
   let difficulties;
   let limit;
+
   if (board.length == 0) {
     try {
       for (let i = 0; i < 15; i++) {
@@ -21,76 +27,91 @@ app.get("/", async (req, res) => {
             categories = "categories=history";
             difficulties = "difficulties=easy";
             limit = "limit=1";
+            score = 100;
             break;
           case 1:
             categories = "categories=history";
             difficulties = "difficulties=medium";
             limit = "limit=1";
+            score = 200;
             break;
           case 2:
             categories = "categories=history";
             difficulties = "difficulties=hard";
             limit = "limit=1";
+            score = 300;
             break;
           case 3:
             categories = "categories=music";
             difficulties = "difficulties=easy";
             limit = "limit=1";
+            score = 100;
             break;
           case 4:
             categories = "categories=music";
             difficulties = "difficulties=medium";
             limit = "limit=1";
+            score = 200;
             break;
           case 5:
             categories = "categories=music";
             difficulties = "difficulties=hard";
             limit = "limit=1";
+            score = 300;
             break;
           case 6:
             categories = "categories=science";
             difficulties = "difficulties=easy";
             limit = "limit=1";
+            score = 100;
             break;
           case 7:
             categories = "categories=science";
             difficulties = "difficulties=medium";
             limit = "limit=1";
+            score = 200;
             break;
           case 8:
             categories = "categories=science";
             difficulties = "difficulties=hard";
             limit = "limit=1";
+            score = 300;
             break;
           case 9:
             categories = "categories=geography";
             difficulties = "difficulties=easy";
             limit = "limit=1";
+            score = 100;
             break;
           case 10:
             categories = "categories=geography";
             difficulties = "difficulties=medium";
             limit = "limit=1";
+            score = 200;
             break;
           case 11:
             categories = "categories=geography";
             difficulties = "difficulties=hard";
             limit = "limit=1";
+            score = 300;
             break;
           case 12:
             categories = "categories=film_and_tv";
             difficulties = "difficulties=easy";
             limit = "limit=1";
+            score = 100;
             break;
           case 13:
             categories = "categories=film_and_tv";
             difficulties = "difficulties=medium";
             limit = "limit=1";
+            score = 200;
             break;
           case 14:
             categories = "categories=film_and_tv";
             difficulties = "difficulties=hard";
             limit = "limit=1";
+            score = 300;
             break;
         }
         let request = await axios.get(
@@ -100,44 +121,59 @@ app.get("/", async (req, res) => {
         board.push({
           question: response[0].question.text,
           answer: response[0].correctAnswer,
+          score: score,
         });
       }
 
       console.log(board);
-      res.render("index.ejs");
+      res.render("index.ejs", { player1: player1Score, player2: player2Score });
     } catch (error) {
       console.log("Failed to connect to server: " + error.message);
     }
   } else {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+      player1: player1Score,
+      player2: player2Score,
+    });
   }
 });
 
 app.get("/question", (req, res) => {
   console.log(req.body);
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    player1: player1Score,
+    player2: player2Score,
+  });
+});
+
+app.post("/question", (req, res) => {
+  chosenQuestion = req.body.question;
+  res.render("question.ejs", {
+    question: board[chosenQuestion].question,
+  });
 });
 
 app.get("/check", (req, res) => {
   let userAnswer = req.query.userAnswer;
-  let answer = req.query.answer;
+  let answer = board[chosenQuestion].answer;
   console.log(userAnswer.toUpperCase());
   console.log(answer.toUpperCase());
   console.log(userAnswer.toUpperCase() === answer.toUpperCase());
   if (userAnswer.toUpperCase() === answer.toUpperCase()) {
-    console.log("YES");
+    if (playerTurn == 1) {
+      player1Score += board[chosenQuestion].score;
+    } else {
+      player2Score += board[chosenQuestion].score;
+    }
   } else {
-    console.log("NO");
+    console.log("WRONG ANSWER");
+    if (playerTurn == 1) {
+      playerTurn = 2;
+    } else {
+      playerTurn = 1;
+    }
   }
-  res.render("index.ejs");
-});
-
-app.post("/question", (req, res) => {
-  const currentQuestion = req.body.question;
-  res.render("question.ejs", {
-    question: board[currentQuestion].question,
-    answer: board[currentQuestion].answer,
-  });
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
